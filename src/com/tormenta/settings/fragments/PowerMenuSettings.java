syberia-project/platform_packages_apps_SyberiaSettings

Add PowerMenu fragment
and move advanced reboot switch into it

Signed-off-by: DennySPB <dennyspb@gmail.com>
Change-Id: Ic73dc84b802617558f372d12cfae1f108f5f55fd

src/com/syberia/settings/fragments/PowerMenuSettings.java
@@ -1,0 +1,42 @@
+/*
+ * Copyright © 2018-2019 Syberia Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.syberia.settings.fragments;
+
+import android.os.Bundle;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+public class PowerMenuSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        addPreferencesFromResource(R.xml.powermenu_settings);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.SYBERIA;
+    }
+}

LLuviaOS/android_packages_apps_ToRmEnTa

Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
Signed-off-by: Srijith Bharadwaj <srijithdamerla2001@gmail.com>

src/com/tormenta/settings/fragments/PowerMenuSettings.java
@@ -1,0 +1,42 @@
+/*
+ * Copyright © 2018-2019 Syberia Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tormenta.settings.fragments;
+
+import android.os.Bundle;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+public class PowerMenuSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        addPreferencesFromResource(R.xml.powermenu_settings);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.TORMENTA;
+    }
+}